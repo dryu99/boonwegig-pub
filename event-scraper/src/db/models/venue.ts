e159dac3fe9cfc83701c4396a2f0912744ea996d
Daniel Ryu <rujjiryu99@gmail.com>
2023-10-10 03:03:55 +0900

    implement venue insert db logic

--- event-scraper/src/db/models/venue.ts
+++ event-scraper/src/db/models/venue.ts
@@ -1,8 +1,21 @@
+import { Insertable, Selectable } from "kysely";
 import { ReviewStatus } from "../../utils/types";
+import { MusicArtist, Venue } from "../db";
+import { DatabaseManager } from "../db-manager";
 
-export type Venue = {
-  instagramId: string;
-  reviewStatus: ReviewStatus;
-  name?: string;
-  address?: string;
-};
+export type NewVenue = Insertable<Venue>;
+export type SavedVenue = Selectable<Venue>;
+
+export class VenueModel {
+  async addOne(newVenue: NewVenue): Promise<SavedVenue | undefined> {
+    return DatabaseManager.db
+      .insertInto("venue")
+      .values(newVenue)
+      .returningAll()
+      .executeTakeFirst();
+  }
+
+  async addMany(newVenues: NewVenue[]) {
+    return DatabaseManager.db.insertInto("venue").values(newVenues).execute();
+  }
+}
