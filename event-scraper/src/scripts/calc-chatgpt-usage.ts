6aa64e28caa4b53801ccfd5fddb5c186fb08dcdf
Daniel Ryu <rujjiryu99@gmail.com>
2023-10-11 16:00:23 +0900

    finish implementing chatgpt cost calculation script

--- event-scraper/src/scripts/calc-chatgpt-usage.ts
+++ event-scraper/src/scripts/calc-chatgpt-usage.ts
@@ -1,6 +1,10 @@
 import fs from "fs";
+import OpenAI from "openai";
 import path from "path";
 import readline from "readline";
+
+const GPT3_INPUT_COST_PER_1K_TOKENS = 0.0015;
+const GPT3_OUTPUT_COST_PER_1K_TOKENS = 0.002;
 
 const main = async () => {
   const logPath = path.resolve(__dirname, `../../logs/chatgpt.development.log`);
@@ -12,22 +16,64 @@ const main = async () => {
     crlfDelay: Infinity, // so that it handles all instances of CR LF ('\r\n') as a single line break
   });
 
-  const logs = [];
+  const usages: OpenAI.Completions.CompletionUsage[] = [];
 
+  let i = 0;
   for await (const line of rl) {
     try {
       const log = JSON.parse(line);
-      logs.push(log);
+      usages.push(log.usage);
     } catch (err) {
-      console.error("Error parsing line:", line, err);
+      console.error("Error parsing line:", { line, err, i });
+    } finally {
+      i++;
     }
   }
 
-  const totalTokens = logs.reduce((prevSum, currLog) => {
-    return prevSum + currLog.usage.total_tokens;
-  }, 0);
+  // const costs = usages.map((usage) => {
+  //   const inputTokensCost =
+  //     (usage.prompt_tokens / 1000) * GPT3_INPUT_COST_PER_1K_TOKENS;
+  //   const outputTokensCost =
+  //     (usage.completion_tokens / 1000) * GPT3_OUTPUT_COST_PER_1K_TOKENS;
 
-  console.log("ChatGPT log results", { logCount: logs.length, totalTokens });
+  //   return inputTokensCost + outputTokensCost;
+  // });
+
+  const inputTokenCost = usages.reduce(
+    (prevSum, currUsage) =>
+      prevSum +
+      (currUsage.prompt_tokens / 1000) * GPT3_INPUT_COST_PER_1K_TOKENS,
+    0
+  );
+
+  const totalInputTokens = usages.reduce(
+    (prevSum, currUsage) => prevSum + currUsage.prompt_tokens,
+    0
+  );
+
+  const outputTokenCost = usages.reduce(
+    (prevSum, currUsage) =>
+      prevSum +
+      (currUsage.completion_tokens / 1000) * GPT3_OUTPUT_COST_PER_1K_TOKENS,
+    0
+  );
+
+  const totalOutputTokens = usages.reduce(
+    (prevSum, currUsage) => prevSum + currUsage.completion_tokens,
+    0
+  );
+
+  console.log("Current ChatGPT usage data", {
+    apiRequestCount: usages.length,
+    inputData: {
+      totalTokens: totalInputTokens,
+      totalCost: "$" + inputTokenCost.toFixed(2),
+    },
+    outputData: {
+      totalTokens: totalOutputTokens,
+      totalCost: "$" + outputTokenCost.toFixed(2),
+    },
+  });
 };
 
 main();
