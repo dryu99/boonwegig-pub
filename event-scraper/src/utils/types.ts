f243d0e78f61fc188e6ae47be3b3a980c05870dc
Daniel Ryu <rujjiryu99@gmail.com>
2023-10-13 20:03:46 +0900

    add comments to review status

--- event-scraper/src/utils/types.ts
+++ event-scraper/src/utils/types.ts
@@ -1,7 +1,7 @@
 export type UUID = string;
 
 export enum ReviewStatus {
-  VALID = "VALID",
-  INVALID = "INVALID",
-  PENDING = "PENDING",
+  VALID = "VALID", // entity is valid and doesn't currently require review
+  INVALID = "INVALID", // entity is invalid and needs to be updated/deleted
+  PENDING = "PENDING", // entity has been created/updated and needs manual review
 }
