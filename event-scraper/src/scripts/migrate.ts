32b755db8158140e32a48bcfa0f78709e3277085
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-09 13:05:50 +0900

    build: install kysely cli package and refactor migration scripts

--- event-scraper/src/scripts/migrate.ts
+++ event-scraper/src/scripts/migrate.ts
@@ -1,36 +1,19 @@
-import * as path from "path";
-import { promises as fs } from "fs";
+import { fs } from "file-system-cache/lib/common";
 import { Migrator, FileMigrationProvider } from "kysely";
+import { run } from "kysely-migration-cli";
+import path from "path";
 import { DatabaseManager } from "../database/db-manager";
 
-const migrateToLatest = async () => {
-  const migrator = new Migrator({
-    db: DatabaseManager.db,
-    provider: new FileMigrationProvider({
-      fs,
-      path,
-      // This needs to be an absolute path.
-      migrationFolder: path.join(__dirname, "../database/migrations"),
-    }),
-  });
+const db = DatabaseManager.db;
 
-  const { error, results } = await migrator.migrateToLatest();
+const migrator = new Migrator({
+  db: DatabaseManager.db,
+  provider: new FileMigrationProvider({
+    fs,
+    path,
+    // This needs to be an absolute path.
+    migrationFolder: path.join(__dirname, "../database/migrations"),
+  }),
+});
 
-  results?.forEach((it) => {
-    if (it.status === "Success") {
-      console.log(`migration "${it.migrationName}" was executed successfully`);
-    } else if (it.status === "Error") {
-      console.error(`failed to execute migration "${it.migrationName}"`);
-    }
-  });
-
-  if (error) {
-    console.error("failed to migrate");
-    console.error(error);
-    process.exit(1);
-  }
-
-  await DatabaseManager.stop();
-};
-
-migrateToLatest();
+run(db, migrator, path.join(__dirname, "../database/migrations"));
