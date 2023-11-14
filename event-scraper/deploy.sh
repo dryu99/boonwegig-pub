e8ab070cf1607d8a6b397c670a820221dffb9191
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-14 20:12:09 +0900

    build(scraper): empty push for CI

--- event-scraper/deploy.sh
+++ event-scraper/deploy.sh
@@ -4,6 +4,6 @@ npm install
 npm run build
 
 # Restart the scraper service
-# sudo systemctl restart event-scraper-service
+# #sudo systemctl restart event-scraper-service
 
 echo "boon-we-gig scraper deployment complete."
