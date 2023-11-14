d0fd3c059c941c0daf91b5125de8fbb15d0bd282
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-15 03:20:46 +0900

    feat(client): revert client component changes + adjust some text formatting

--- client/src/app/lib/date.helper.ts
+++ client/src/app/lib/date.helper.ts
@@ -2,24 +2,24 @@ export class DateHelper {
   private static readonly DAYS_OF_WEEK = [
     "Sun",
     "Mon",
-    "Tues",
+    "Tue",
     "Wed",
-    "Thurs",
+    "Thu",
     "Fri",
     "Sat",
   ];
 
   public static extractParts(date: Date): {
     dayOfWeek: string;
-    day: number;
-    month: number;
-    year: number;
+    day: string;
+    month: string;
+    year: string;
     time: string;
   } {
     const dayOfWeek = this.getDayOfWeek(date.getDay());
-    const day = date.getDate();
-    const month = date.getMonth() + 1;
-    const year = date.getFullYear();
+    const day = date.getDate().toString().padStart(2, "0");
+    const month = (date.getMonth() + 1).toString().padStart(2, "0");
+    const year = date.getFullYear().toString();
     const time = date.toLocaleTimeString([], {
       hour: "2-digit",
       minute: "2-digit",
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,7 @@ import {
   ClientMusicEvent,
   DatabaseManager,
 } from "./lib/database/db-manager";
-import { EventDate } from "./ui/event-date";
+import { DateHelper } from "./lib/date.helper";
 
 export default async function Home() {
   const musicEvents = await DatabaseManager.getAllUpcomingMusicEvents();
@@ -21,12 +21,32 @@ export default async function Home() {
 }
 
 const MusicEvent = ({ musicEvent }: { musicEvent: ClientMusicEvent }) => {
+  const startDateParts = DateHelper.extractParts(musicEvent.startDateTime);
+
+  // TODO fix issue where date is not aligned with other columns
   return (
     <div className="flex m-1">
-      <EventDate
-        startDate={musicEvent.startDateTime}
-        createDate={musicEvent.createdAt}
-      />
+      <div className="flex-none p-1 w-36">
+        <div>
+          <span className="text-2xl font-bold">
+            {startDateParts.month}/{startDateParts.day}
+          </span>{" "}
+          ({startDateParts.dayOfWeek})
+        </div>
+        <div>
+          <span className="mr-2">{startDateParts.time}</span>
+          <span className="mr-2">
+            {/* TODO Long term we shouldn't do this, should prob use enum or sth  */}
+            {musicEvent.eventType === "CONCERT" ? "🎸" : "🎧"}
+          </span>
+          {musicEvent.isFree && (
+            <span className="text-yellow-400 mr-2">Free</span>
+          )}
+          {DateHelper.isRecent(musicEvent.createdAt) && (
+            <span className="text-blue-400">New</span>
+          )}
+        </div>
+      </div>
       <div className="flex-none p-1 w-36">
         <div>
           <a
