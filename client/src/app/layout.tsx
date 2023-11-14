0445177c582f8aa9263540aa92e69056600d61de
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-15 04:38:05 +0900

    feat(client): style up frontend again

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${courier.className} antialiased`}>
         <div className="flex flex-col items-center">
-          <h1 className="text-2xl p-8 pb-4 font-bold">BoonWeGig</h1>
+          <h1 className="text-2xl m-8 mb-4 font-bold">BoonWeGig</h1>
           <CityPicker />
         </div>
 
