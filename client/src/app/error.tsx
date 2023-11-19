3cfd91a17d634cc0569ac1fc3c184b21d4c0bfbe
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-20 00:57:24 +0900

    feat(client): adjust and add error pages

--- client/src/app/error.tsx
+++ client/src/app/error.tsx
@@ -1,17 +1,10 @@
 "use client";
 
-import { useEffect } from "react";
-
 export default function Error({
   error,
 }: {
   error: Error & { digest?: string };
 }) {
-  useEffect(() => {
-    // Optionally log the error to an error reporting service
-    console.error(error);
-  }, [error]);
-
   return (
     <main className="flex h-full flex-col items-center justify-center">
       <h2 className="text-center">Something went wrong ㅠ _ ㅠ</h2>
