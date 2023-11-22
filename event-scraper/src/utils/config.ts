cb6a7ad480f1f1dad2026810d6a8fa7f606175c2
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-22 19:51:11 +0900

    chore(scraper): remove unused env vars

--- event-scraper/src/utils/config.ts
+++ event-scraper/src/utils/config.ts
@@ -18,18 +18,13 @@ dotenv.config({
 
 export const Config = Object.freeze({
   NODE_ENV: process.env.NODE_ENV as "development" | "production",
-  INSTAGRAM_COOKIE: ensure(process.env.INSTAGRAM_COOKIE),
-  INSTAGRAM_USER_AGENT: ensure(process.env.INSTAGRAM_USER_AGENT),
-  INSTAGRAM_X_IG_APP_ID: ensure(process.env.INSTAGRAM_X_IG_APP_ID),
   OPENAI_API_KEY: ensure(process.env.OPENAI_API_KEY),
   DATABASE_HOST: ensure(process.env.DATABASE_HOST),
   DATABASE_PORT: ensure(process.env.DATABASE_PORT),
   DATABASE_USER: ensure(process.env.DATABASE_USER),
   DATABASE_PASSWORD: ensure(process.env.DATABASE_PASSWORD),
   DATABASE_NAME: ensure(process.env.DATABASE_NAME),
-  VITE_SPOTIFY_CLIENT_SECRET: ensure(process.env.VITE_SPOTIFY_CLIENT_SECRET),
-  VITE_SPOTIFY_CLIENT_ID: ensure(process.env.VITE_SPOTIFY_CLIENT_ID),
-  VITE_REDIRECT_TARGET: ensure(process.env.VITE_REDIRECT_TARGET),
+
   SENTRY_DSN: ensure(process.env.SENTRY_DSN),
   SCRAPING_FISH_API_KEY: ensure(process.env.SCRAPING_FISH_API_KEY),
 });
