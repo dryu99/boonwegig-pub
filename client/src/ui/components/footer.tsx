2eaab45fb6647458ae75321addbafe152d29a7cb
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-27 20:10:37 +0900

    feat(client): add disclaimer note to footer

--- client/src/ui/components/footer.tsx
+++ client/src/ui/components/footer.tsx
@@ -1,6 +1,11 @@
+import { useTranslations } from "next-intl";
+
 export const Footer = () => {
+  const t = useTranslations("Footer");
+
   return (
-    <div className="text-center text-sm mt-10 mb-4">
+    <div className="text-center text-sm mt-10 mb-4 flex flex-col items-center">
+      <div className="w-[50em] text-secondary mb-2">{t("note")}</div>
       <div>© 2023 BoonWeGig</div>
       <div>contact: boonwegig@gmail.com</div>
     </div>
