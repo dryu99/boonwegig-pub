c39df57bceed4be617243a00c5f28040bb5bae0f
Daniel Ryu <rujjiryu99@gmail.com>
2023-11-30 22:34:42 +0900

    feat(client): update sitemap

--- client/src/app/sitemap.ts
+++ client/src/app/sitemap.ts
@@ -4,16 +4,40 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://boonwegig.com/en",
+      url: "https://boonwegig.com/en/seoul",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
     },
     {
-      url: "https://boonwegig.com/ko",
+      url: "https://boonwegig.com/ko/seoul",
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
+    },
+    {
+      url: "https://boonwegig.com/en/busan",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://boonwegig.com/ko/busan",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+    {
+      url: "https://boonwegig.com/en",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
+    },
+    {
+      url: "https://boonwegig.com/ko",
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.8,
     },
   ];
 }
